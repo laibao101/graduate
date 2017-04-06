7517fce8d521ae3a3ba4eee02dcce26eee0f4334
laibao101 <369632567@qq.com>
2017-04-06 18:33:47 +0800

    optimiz

--- toilet/ios_views/TWebView.js
+++ toilet/ios_views/TWebView.js
@@ -6,17 +6,47 @@ class TWebView extends Component{
 		super(props);
 
 		this.state = {
-			url:this.props.url
+			url:this.props.url,
+			isError:false
 		}
 	}
 
 	render(){
 		return (
-			<View>
-				<WebView source={{uri:this.state.url}}></WebView>
+			<View style={styles.container}>
+				{
+					this.state.isError ?
+					<Text style={styles.errorInfo}>网络有问题，请检查网络设置并刷新</Text>
+					:
+					<WebView
+						onError = {this._showError.bind(this)}
+						style = {{marginTop:-20}}
+						source = {{uri:this.state.url}}
+						startInLoadingState = {true}
+					></WebView>
+				}
+
+
 			</View>
 		);
 	}
+
+	_showError(){
+		this.setState({
+			isError:true
+		});
+	}
 }
+
+const styles = StyleSheet.create({
+	container:{
+		flex:1
+	},
+	errorInfo:{
+		marginTop:100,
+		justifyContent:'center',
+		alignItems:'center'
+	}
+})
 
 export default TWebView;
